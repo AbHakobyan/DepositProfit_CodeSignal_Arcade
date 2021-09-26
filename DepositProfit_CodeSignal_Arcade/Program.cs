816b94a301995c2b1f1e7166ab05f1a0c27c576e
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-27 01:50:16 +0400

    Добавьте файлы проекта.

---
+++ DepositProfit_CodeSignal_Arcade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepositProfit_CodeSignal_Arcade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = 100;
+            int r = 20;
+            int t = 170;
+            int res = depositProfit(d, r, t);
+            Console.WriteLine(res);
+        }
+
+        static int depositProfit(int deposit, int rate, int threshold)
+        {
+            int count = 0;
+            while(deposit < threshold)
+            {
+                deposit = ((deposit * rate) / 100 ) + deposit;
+                count++;
+            }    
+            return count;
+        }
+    }
+}
